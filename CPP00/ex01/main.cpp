aac709d344de88e4ae20b44d4f49560affc5feb1
ByteGab <gabrielfreiresimon@gmail.com>
2025-10-08 10:28:19 +0200

    feature/ create menu

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 10:39:56 by gafreire          #+#    #+#             */
-/*   Updated: 2025/10/07 17:05:43 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/10/08 10:26:40 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 int main (void)
 {
-    Phonebook libro;
-    // libro.("Gabriel","Freire","gafreire","6238420424","gallego");
-
-    libro.add_contact();
+    Phonebook phonebook;
+    std::string menu;
+    
+    while (menu != "EXIT")
+    {
+        std::getline(std::cin, menu);
+        if (menu == "ADD")
+            phonebook.add_contact();
+        else if (menu == "SEARCH")
+            phonebook.search_contact();
+        if (std::cin.eof())
+        {
+            std::cout << std::endl;
+            return (0);
+        }
+    }
     return (0);
 }
