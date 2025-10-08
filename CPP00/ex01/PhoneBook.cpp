29cad20e9ff9bea730debfe01dd21d87ce877036
ByteGab <gabrielfreiresimon@gmail.com>
2025-10-08 10:28:36 +0200

    feature/ create search contact

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 12:20:42 by gafreire          #+#    #+#             */
-/*   Updated: 2025/10/07 18:50:51 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/10/08 10:23:21 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void Phonebook::add_contact(void)
 {
-    Phonebook libro;
     std::string prueba;
 
     prueba = "";    
@@ -25,51 +24,74 @@ void Phonebook::add_contact(void)
     {
         std::cout << "Vuelve a introducir un nombre valido" << std::endl;
     }
-    libro.contacts[0].set_first_name(prueba);
+    this->contacts[0].set_first_name(prueba);
         
     std::cout << "Introduce los apellidos" << std::endl;
     while (!std::getline(std::cin,prueba)|| prueba.empty())
     {
         std::cout << "Vuelve a introducir unos apellidos validos" << std::endl;
     }
-    libro.contacts[0].set_last_name(prueba);
+    this->contacts[0].set_last_name(prueba);
     
     std::cout << "Introduce un nickname" << std::endl;
     while (!std::getline(std::cin,prueba)|| prueba.empty())
     {
         std::cout << "Vuelve a introducir un nickname valido" << std::endl;
     }
-    libro.contacts[0].set_nickname(prueba);
+    this->contacts[0].set_nickname(prueba);
     
     std::cout << "Introduce un numero de telefono" << std::endl;
     while (!std::getline(std::cin,prueba)|| prueba.empty())
     {
         std::cout << "Vuelve a introducir un numero de telefono" << std::endl;
     }
-    libro.contacts[0].set_phone_number(prueba);
+    this->contacts[0].set_phone_number(prueba);
     
     std::cout << "Introduce un dark secret" << std::endl;
     while (!std::getline(std::cin,prueba)|| prueba.empty())
     {
         std::cout << "Vuelve a introducir un numero de telefono" << std::endl;
     }
-    libro.contacts[0].set_darkest_secret(prueba);
+    this->contacts[0].set_darkest_secret(prueba);
 
-    std::cout << libro.contacts[0].get_first_name() << libro.contacts[0].get_last_name() << libro.contacts[0].get_nickname() << libro.contacts[0].get_phone_number() << libro.contacts[0].get_darkest_secret() << std::endl;
+    std::cout << this->contacts[0].get_first_name() << this->contacts[0].get_last_name() << this->contacts[0].get_nickname() << this->contacts[0].get_phone_number() << this->contacts[0].get_darkest_secret() << std::endl;
+    this->count++;
 }
 void Phonebook::search_contact(void)
 {
     int i;
+    std::string prueba;
 
     i = 0;
-    if (this->index == 0)
+    if (this->count == 0)
+    {
         std::cout << "PhoneBook empty" << std::endl;
-    while (i < this->index)
+        return ;
+    }
+    std::cout << " ___________________________________________ " << std::endl;
+    std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+    std::cout << "|----------|----------|----------|----------|" << std::endl;
+    while (i < this->count)
     {
          std::cout << this->contacts[i].get_first_name() << " " << this->contacts[i].get_last_name() << " " <<  this->contacts[i].get_nickname() << " " <<  this->contacts[i].get_phone_number() << " " <<  this->contacts[i].get_darkest_secret()  <<  " " << std::endl;
         i++;
     }
-    this->index++;
+
+    std::cout << "Selecciona un contacto" << std::endl;
+    i = 0;
+     while (!std::cin.eof())
+     {
+        std::cout << "Select a index contact" << std::endl;
+        if (std::getline(std::cin, prueba) && prueba != "")
+        {
+            if (prueba.size() == 1 && prueba[0] >= '1' && prueba[0] <= '8' && this->contacts[prueba[0] - 1 - '0'].get_first_name().size())
+                break;
+        }
+        else
+            std::cout << "Invalid index contact" << std::endl;         
+     }
+    //  if (!std::cin.eof())
+        
 }
 
 Phonebook::Phonebook()
