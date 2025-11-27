1a19cf3980ddd8a5cba767edd7d089179a2ca5a3
ByteGab <gabrielfreiresimon@gmail.com>
2025-11-27 17:52:46 +0100

    feature/ add example randomChump

--- CP01/ex00/main.cpp
+++ CP01/ex00/main.cpp
@@ -6,18 +6,21 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 17:55:55 by gafreire          #+#    #+#             */
-/*   Updated: 2025/11/27 17:44:28 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/11/27 17:51:16 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* newZombie( std::string name);
+void randomChump( std::string name);
 
 int main (void)
 {
     Zombie *zombie = newZombie("Foo");
     zombie->announce();
     zombie->~Zombie();
+
+    randomChump("Andrei");
     return (0);
 }
