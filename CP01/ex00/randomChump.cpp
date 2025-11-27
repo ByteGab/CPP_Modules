ed49796ba0a6193aaa55a5630e07ec08ddf1e8e7
ByteGab <gabrielfreiresimon@gmail.com>
2025-11-27 17:46:24 +0100

    feature/ create randomChump

---
+++ CP01/ex00/randomChump.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 18:05:10 by gafreire          #+#    #+#             */
+/*   Updated: 2025/11/27 16:27:08 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void randomChump( std::string name)
+{
+    Zombie zombie = Zombie(name);
+    zombie.announce();
+}
