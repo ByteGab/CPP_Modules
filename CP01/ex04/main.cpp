2888bfdf5441db7871065e2514eb4d3d0fc0ceb1
ByteGab <gabrielfreiresimon@gmail.com>
2025-12-11 17:03:47 +0100

    feature/ finish ex04

--- CP01/ex04/main.cpp
+++ CP01/ex04/main.cpp
@@ -6,12 +6,14 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 14:53:38 by gafreire          #+#    #+#             */
-/*   Updated: 2025/12/10 16:02:38 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/12/11 17:02:07 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main (int argc, char *argv[])
 {
@@ -20,5 +22,33 @@ int main (int argc, char *argv[])
         std::cout << "Error arguments invalids" << std::endl;
         return (1);
     }
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    std::ifstream file(filename.c_str());
+    if (!file.is_open())
+    {
+        std::cout << "Error file" << std::endl;
+        return (1);
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string text = buffer.str();
+    file.close();
+    if (!s1.empty())
+    {
+        size_t pos = text.find(s1);
+        while (pos != std::string::npos)
+        {
+            text.erase(pos, s1.length());
+            text.insert(pos, s2);
+            pos = text.find(s1, pos + s2.length());
+        }
+    }
+    std::ofstream outfile((filename + ".replace").c_str());
+    if (!outfile.is_open()) 
+        return (1);
+    outfile << text;
+    outfile.close();
     return (0);
 }
