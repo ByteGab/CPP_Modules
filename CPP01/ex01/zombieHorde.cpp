48bd3c5e10ad0c4e878c55a852254e3714f8e563
ByteGab <gabrielfreiresimon@gmail.com>
2025-12-12 17:45:21 +0100

    feature/ refactor main

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 15:33:24 by gafreire          #+#    #+#             */
-/*   Updated: 2025/12/01 15:53:24 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/12/12 17:44:42 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 Zombie* zombieHorde( int N, std::string name )
 {
     Zombie *zombies = new Zombie[N];
+    
     for(int i = 0; i < N; i++)
-    {
         zombies[i].setName(name);
-    } 
+        
     return (zombies);
 }
