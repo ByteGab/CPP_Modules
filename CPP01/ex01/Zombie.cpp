96c0b476bdf97e08c9a335627cf439e480674a30
ByteGab <gabrielfreiresimon@gmail.com>
2025-12-12 17:43:33 +0100

    feature/ add colors print

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -6,17 +6,23 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 15:33:14 by gafreire          #+#    #+#             */
-/*   Updated: 2025/12/01 15:51:09 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/12/12 17:41:10 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
 
 void Zombie::announce(void)
 {
-        std::cout << this->_name 
-                  << ": BraiiiiiiinnnzzzZ..." 
-                  << std::endl;
+        std::cout << MAGENTA << this->_name << RESET 
+              << ": " << CYAN << "BraiiiiiiinnnzzzZ..." << RESET 
+              << std::endl;
 }
 void Zombie::setName(std::string name)
 {
@@ -24,14 +30,14 @@ void Zombie::setName(std::string name)
 }
 Zombie::Zombie()
 {
+    std::cout << MAGENTA << "Zombie" << RESET
+        << RESET << ": "<< GREEN <<"created" << RESET 
+            << std::endl;
 }
-// Zombie::Zombie(std::string name)
-// {
-//     this->_name = name;
-// }
+
 Zombie::~Zombie()
 {
-    std::cout << this->_name 
-                  << ": destroyed" 
-                  << std::endl;
+    std::cout << MAGENTA << this->_name << RESET 
+              << ": " << RED << "destroyed" << RESET 
+              << std::endl;
 }
