d6c8396ca90478220fca175101fdee6a3be0d134
ByteGab <gabrielfreiresimon@gmail.com>
2026-01-13 15:49:01 +0100

    feature/ finish ex01

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:19:19 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/13 12:13:29 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/01/13 12:18:23 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,17 @@ Fixed::Fixed() : _value(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->_value = value << _fractionalBits;
+}
 
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->_value = roundf(value *(1 << _fractionalBits));
+}
 Fixed::Fixed(const Fixed &cpy)
 {
     std::cout << "Copy constructor called" << std::endl;
@@ -39,10 +49,25 @@ Fixed::~Fixed()
 void Fixed::setRawBits(int const raw)
 {
     this->_value = raw;
-}
 
+}
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
     return (this->_value);
+}
+
+float Fixed::toFloat(void) const
+{
+    return ((float)this->_value / (float)(1 << _fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->_value >> _fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+    out << fixed.toFloat();
+    return (out);
 }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:19:22 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/13 12:13:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/01/13 12:12:42 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 #define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
 private:
-    /* data */
     int _value;
     static const int _fractionalBits = 8;
 public:
-    // constructor
+    // constructor default
     Fixed();
+    // constructor int
+    Fixed(const int value);
+    // constructor float
+    Fixed(const float value);
+    // copy constructor
     Fixed(const Fixed &cpy);
     // operator overload
     Fixed &operator=(const Fixed &cpy);
     // destructor
-    ~Fixed(); 
+    ~Fixed();
     // setter
-     void setRawBits(int const raw);
+    void setRawBits(int const raw);
     // getter
     int  getRawBits(void) const;
+
+    float toFloat(void) const;
+    int toInt(void) const;
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
 
 #endif
