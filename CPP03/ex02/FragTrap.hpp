20ef2bd1353c1540a750f9269641aa55464193d8
ByteGab <gabrielfreiresimon@gmail.com>
2026-01-22 18:31:12 +0100

    fix/ errors ex02 evals

--- CPP03/ex02/FragTrap.hpp
+++ CPP03/ex02/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 15:52:37 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/20 16:01:13 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/01/22 18:09:40 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,20 @@
 #include "ClapTrap.hpp"
 
 class FragTrap : public ClapTrap
-{
-private:
-    /* data */
-public:
-    // constructor default
-    FragTrap(void);
-    // constructor
-    FragTrap(std::string name);
-    // copy Constructor
-    FragTrap(const FragTrap &cpy);
-    // operator overload
-    FragTrap &operator=(const FragTrap &cpy);
-    // destructor
-    ~FragTrap();
-
-    void highFivesGuys(void);
+{   
+    public:
+        // constructor default
+        FragTrap(void);
+        // constructor
+        FragTrap(std::string name);
+        // copy Constructor
+        FragTrap(const FragTrap &cpy);
+        // operator overload
+        FragTrap &operator=(const FragTrap &cpy);
+        // destructor
+        ~FragTrap();
+    
+        void highFivesGuys(void);
 };
 
 
--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 19:56:32 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/21 11:30:15 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/01/22 18:30:28 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 
 int main (void)
 {
-    // Test 1
     std::cout << std::endl << MAGENTA << "=== " << "TEST 1: CONSTRUCTORS " << " ===" << RESET << std::endl;
     std::cout << YELLOW << "[Creating Bender]" << RESET << std::endl;
     FragTrap robot("Bender");
     
-    // Test 2
     std::cout << std::endl << MAGENTA << "=== " << "TEST 2: STATS AND ATTACK)" << " ===" << RESET << std::endl;
     robot.attack("Target Dummy");
     robot.takeDamage(20);
     robot.beRepaired(10);
     
-    // Test 3
     std::cout << std::endl << MAGENTA << "=== " << "TEST 3: (HIGH FIVES)" << " ===" << RESET << std::endl;
     robot.highFivesGuys();
     
-    // Test 4
     std::cout << YELLOW << "[Testing the Copy Constructor: Cloning Bender]" << RESET << std::endl;
     FragTrap clon(robot);
     
