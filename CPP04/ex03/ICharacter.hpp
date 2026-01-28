f85d73b747e8cc83827fada3d52bbc701aa48011
ByteGab <gabrielfreiresimon@gmail.com>
2026-01-28 22:45:17 +0100

    feaute/ finish ICharacter

--- CPP04/ex03/ICharacter.hpp
+++ CPP04/ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 19:54:37 by gafreire          #+#    #+#             */
-/*   Updated: 2026/01/28 19:56:41 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:44:43 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class AMateria;
 class ICharacter
 {
     public:
-    	virtual	~ICharacter(void);
+    	virtual	~ICharacter(void) {};
     	virtual std::string const	&getName() const = 0;
     	virtual void	equip(AMateria *m) = 0;
     	virtual void	unequip(int idx) = 0;
