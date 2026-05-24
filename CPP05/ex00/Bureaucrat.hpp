c55d5cd9dd2bba2c7cd392517250b804a52e4453
ByteGab <gabrielfreiresimon@gmail.com>
2026-05-24 18:49:43 +0200

    feature/add Bureaucrat class implementation and tests

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 17:42:01 by gafreire          #+#    #+#             */
+/*   Updated: 2026/05/24 18:10:58 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class  Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grade;
+        
+    public:
+        // constructor default
+        Bureaucrat(void);
+        // assignment operator
+        Bureaucrat(const Bureaucrat &cpy);
+        Bureaucrat(std::string const &name, int grade);
+        // operator overload
+        Bureaucrat &operator=(const Bureaucrat &cpy);
+        // destructor
+        ~Bureaucrat();
+        std::string getName() const;      
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();           
+        };
+};
+std::ostream &operator<<(std::ostream &src, Bureaucrat const &other);
+
+#endif
--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,67 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 17:41:08 by gafreire          #+#    #+#             */
-/*   Updated: 2026/03/02 17:41:09 by gafreire         ###   ########.fr       */
+/*   Updated: 2026/05/24 18:41:25 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+#define RESET   "\033[0m"
+#define CYAN    "\033[36m"
+
+
+int main ()
+{
+    std::cout << CYAN << "--- TEST 1: Valid bureaucrat creation and increment ---" << RESET << std::endl;
+    try 
+    {
+        Bureaucrat pepe("Pepe", 50);
+        std::cout << pepe << std::endl;
+        
+        pepe.incrementGrade();
+        std::cout << pepe << std::endl;
+    }
+    catch (std::exception &e) 
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    std::cout << CYAN << "\n--- TEST 2: Exception - Creating bureaucrat with grade too high (0) ---" << RESET << std::endl;
+    try 
+    {
+        Bureaucrat andrei("Andrei", 0);
+        std::cout << andrei << std::endl;
+        // Intenta instanciar un Bureaucrat con grado 0 aquí
+    }
+    catch (std::exception &e) 
+    {
+        std::cerr << "Caught error: " << e.what() << std::endl;
+    }
+    std::cout << CYAN << "\n--- TEST 3: Exception - Incrementing bureaucrat with grade 1 ---" << RESET << std::endl;
+    try 
+    {
+        Bureaucrat daniel("Daniel", 1);
+        std::cout << daniel << std::endl;
+        
+        daniel.incrementGrade();
+    }
+    catch (std::exception &e) 
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    std::cout << CYAN << "\n--- TEST 4: Exception - Decrementing bureaucrat with grade 150 ---" << RESET << std::endl;
+    try 
+    {
+        Bureaucrat paco("Paco", 150);
+        std::cout << paco << std::endl;
+        
+        paco.decrementGrade();
+        std::cout << paco << std::endl;
+    }
+    catch (std::exception &e) 
+    {
+        std::cerr << "Caught error: " << e.what() << std::endl;
+    }
+    
+    return (0);
+}
